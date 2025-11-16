85ac2b86ae73050c1f74527e32d37d6b604dd20d
Tomas Oliver <tomi204@outlook.com>
2025-11-16 20:55:01 -0300

    fallback for mainnet

--- frontend/src/lib/solana.ts
+++ frontend/src/lib/solana.ts
@@ -1,6 +1,6 @@
 import { Connection, clusterApiUrl } from '@solana/web3.js';
 
-export const SOLANA_NETWORK = 'devnet';
+export const SOLANA_NETWORK = 'mainnet-beta';
 export const SOLANA_RPC_URL = process.env.NEXT_PUBLIC_SOLANA_RPC_URL || clusterApiUrl(SOLANA_NETWORK);
 
 export const connection = new Connection(SOLANA_RPC_URL, 'confirmed');
